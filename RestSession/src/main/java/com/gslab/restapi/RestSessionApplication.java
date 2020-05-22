1e713d457305180bd8ad6f705a545cd560280308
Ishwari <itpednekar16@gmail.com>
2020-05-22 10:43:30 +0530

    ishwari project

---
+++ RestSession/src/main/java/com/gslab/restapi/RestSessionApplication.java
@@ -0,0 +1,15 @@
+package com.gslab.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+
+public class RestSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSessionApplication.class, args);
+	}
+
+}
