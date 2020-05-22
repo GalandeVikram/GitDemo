bea815b552c8d05d8bb34aecfc8286320115826e
Unkn0wnerr <prasad17011997@gmail.com>
2020-05-22 11:03:59 +0530

    prasad calculator file.

--- gitdemo/src/main/java/com/gslab/gitdemo/calculator.java
+++ gitdemo/src/main/java/com/gslab/gitdemo/calculator.java
@@ -1,5 +1,6 @@
+package com.gslab.gitdemo;
 public class calculator(){
-    public static void main(String[] args) {
+    public static void calc{
 
         System.out.println(4+4);
         System.out.println(4*4);
