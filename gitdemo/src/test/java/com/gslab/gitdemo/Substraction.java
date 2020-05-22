38abfd639f6eb87a0fb6ac9118bec0cdd31469e2
sahdevsingh <sahdev.akhawat.15@gmail.com>
2020-05-22 08:50:03 +0530

    sahdev commit

---
+++ gitdemo/src/test/java/com/gslab/gitdemo/Substraction.java
@@ -0,0 +1,16 @@
+package com.gslab.gitdemo;
+
+public class Substraction {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int val=10;
+		int val1=20;
+		int sum =val+val1;
+		System.out.println(sum);
+
+	}
+
+}
