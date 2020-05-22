58a06623ded71ddc767302fd81a29c3734c5fb33
gvikram <gvikram@avaya.com>
2020-05-22 09:44:06 +0530

    Add sample project

---
+++ gitdemo/src/main/java/com/gslab/gitdemo/App.java
@@ -0,0 +1,13 @@
+package com.gslab.gitdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
